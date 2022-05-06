535c5bd32b553eb2ca7e645815873195dcd7ccb5
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-05-06 06:36:31 -0700

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { Button } from 'carbon-components-react';
+import { Button, Content } from 'carbon-components-react';
+import { Route, Switch } from 'react-router-dom';
 import './app.scss';
+import Header from './components/Header';
+
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Button>Button</Button>
+        <Header />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+          <Button>Button</Button>
+        </Content>
       </>
     );
   }
